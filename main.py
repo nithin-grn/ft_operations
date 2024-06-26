865eaeba55514b8766192f4ade32cbd07473a375
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-26 13:18:43 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-  st.header('FT Local Kitchen & Bar', divider = 'black')
+  st.header('FT Local Kitchen & Bar', divider = 'grey')
   with st.container(height = 200):
     col1, col2 = st.columns([1, 3])
     col1.image('logo.jpg')
