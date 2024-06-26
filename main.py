8cc10ad9913a9bd5df6ee284a542c34e087bfa73
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-26 13:38:54 +1000

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ st.header('FT Local Kitchen & Bar', divider = 'grey')
 st.sidebar.header('FT Local Kitchen & Bar')
 
 def main():
-  
   with st.container(height = 200, border = None):
     col1, col2 = st.columns([1, 3])
     col1.image('logo.jpg')
     col2.image('banner.jpg')
+
+  st.header('Daily Tasks')
+  st.caption('Start your day with the opening tasks.')
+  with st.container(height = 200, border = None):
+    col1, col2 = st.columns(2)
+    col1.image('logo.jpg')
+    col2.image('banner.jpg')
+
+  st.header('Utilities')
+
+
 
 if __name__ == "__main__":
   main()
