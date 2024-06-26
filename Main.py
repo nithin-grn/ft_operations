03c13e7added97d621aca30081f0711fcaf65a33
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-26 19:27:26 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -40,10 +40,21 @@ def main():
       st.switch_page("pages/1_Opening_Tasks.py") if clicked == 0 else (st.switch_page("pages/2_Closing_Tasks.py") if clicked == 1 else None)
 
   st.subheader('Utilities')
-  with st.container(height = 200, border = None):
-    col1, col2 = st.columns(2)
-    col1.image('inventory.jpg', caption = 'Inventory')
-    col2.image('service.jpeg', caption = 'Service Expectations')
+  images = get_clickable_images(['inventory.jpg', 'service.jpeg'])
+  with st.container():
+        # Display clickable images in a flexbox layout
+        clicked = clickable_images(
+            images,
+            titles=[f"Click here for tasks" for i in range(len(images))],
+            div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+            img_style={"margin": "1%", "height": "200px", "width": "48%"}
+        )
+  c1, c2 = st.columns(2)
+  c1.markdown("<div style='text-align: center;'><b>Inventory</b></div>", unsafe_allow_html=True)
+  c2.markdown("<div style='text-align: center;'><b>Service Expectations</b></div>", unsafe_allow_html=True)
+  if clicked > -1:
+      st.switch_page("pages/3_Inventory.py") if clicked == 0 else (st.switch_page("pages/4_Service_Expectations.py") if clicked == 1 else None)
+
 
 
 
