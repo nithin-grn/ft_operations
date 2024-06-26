7b702c74c036713a390713fe3a402124e6308eb6
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-26 18:54:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,19 @@ import streamlit as st
 import base64
 from st_clickable_images import clickable_images
 
-images = []
-for file in ["logo.jpg", "opening.jpg"]:
-    with open(file, "rb") as image:
-        encoded = base64.b64encode(image.read()).decode()
-        images.append(f"data:image/jpeg;base64,{encoded}")
-
-# Create a container
-with st.container():
-    # Display clickable images in a flexbox layout
-    clicked = clickable_images(
-        images,
-        titles=[f"Image #{str(i)}" for i in range(len(images))],
-        div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
-        img_style={"margin": "1%", "height": "200px", "width": "48%"}
-    )
-
-st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
 
 st.logo('logo.jpg')
 st.header('FT Local Kitchen & Bar', divider = 'grey')
 st.sidebar.header('FT Local Kitchen & Bar')
+
+def get_clickable_images(files):
+    images = []
+    for file in files:
+        with open(file, "rb") as image:
+            encoded = base64.b64encode(image.read()).decode()
+            images.append(f"data:image/jpeg;base64,{encoded}")
+    return images
+        
 
 def main():
   with st.container(height = 200, border = None):
@@ -32,10 +24,20 @@ def main():
 
   st.subheader('Daily Tasks')
   st.caption('Start your day with the opening tasks.')
-  with st.container(height = 200, border = None):
-    col1, col2 = st.columns(2)
-    col1.image('opening.jpg', caption = 'Opening Tasks')
-    col2.image('closing.jpg', caption = 'Closing Tasks')
+  images = get_clickable_images(['opening.jpg', 'closing.jpg'])
+  with st.container():
+        # Display clickable images in a flexbox layout
+        clicked = clickable_images(
+            images,
+            titles=[f"Image #{str(i)}" for i in range(len(images))],
+            div_style={"display": "flex", "justify-content": "center", "flex-wrap": "wrap"},
+            img_style={"margin": "1%", "height": "200px", "width": "48%"}
+        )
+  c1, c2 = st.columns(2)
+  c1.write('Opening Tasks')
+  c2.write('Closing Tasks')
+  
+  st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
 
   st.subheader('Utilities')
   with st.container(height = 200, border = None):
