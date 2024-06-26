171eb45698bcd3a6a351eaa666ae2232fb85875d
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-26 13:14:05 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 def main():
-  col1, col2 = st.columns([1, 3])
-  col1.image('logo.jpg')
-  col2.image('banner.jpg')
+  with st.container(height = 200):
+    col1, col2 = st.columns([1, 3])
+    col1.image('logo.jpg')
+    col2.image('banner.jpg')
 
 if __name__ == "__main__":
   main()
