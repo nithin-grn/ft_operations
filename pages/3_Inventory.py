ea58412e1688a28475fad6052a5c52639b514a5e
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 19:05:19 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -4,14 +4,15 @@ def split_list(input_list, chunk_size=5):
     return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
 
 def display_categories():
+  chunk_size = 5
   categories = ['Apple', 'Orange', 'Mango', 'Banana', 'Guava', 'Jackie', 'Passionfruit']
-  list_categories = split_list(categories)
+  list_categories = split_list(categories, chunk_size)
   with st.container():
     for cat in list_categories:
-      cols = st.columns(len(cat))
-      for idx, col in enumerate(cols):
-        col.image('logo.jpg')
-        col.button(cat[idx], use_container_width=True, type = 'primary', key = f'btn_{cat[idx]}')
+      cols = st.columns(chunk_size)
+      for idx, c in enumerate(cat):
+        cols[idx].image('logo.jpg')
+        cols[idx].button(c, use_container_width=True, type = 'primary', key = f'btn_{c}')
 
 if __name__ == "__main__":
   st.header('Inventory')
