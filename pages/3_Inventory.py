d8a5d5a4710ab6ed717ab6ed337f74f0e2abfece
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 18:41:39 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -4,8 +4,8 @@ def display_categories():
   categories = ['Apple', 'Orange', 'Mango', 'Banana', 'Guava', 'Jackie', 'Passionfruit']
   cols = st.columns(len(categories))
   for idx, col in enumerate(cols):
-    col1.image('logo.jpg')
-    col2.write(categories[idx])
+    col.image('logo.jpg')
+    col.write(categories[idx])
 
 if __name__ == "__main__":
   st.header('Inventory')
