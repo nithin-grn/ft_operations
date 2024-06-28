8a4731518318d144baea4ad2b56228d518ae5a0e
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 21:21:41 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -8,6 +8,18 @@ def get_categories(conn, area):
   df = get_df(conn, f'{area}_Inventory')
   return get_columns(df)
 
+def get_items_from_category(df, category):
+    return df[category]
+
+def get_stock_dict(conn, area, category):
+    df = get_df(conn, f'{area}_Inventory')
+    items = get_items_from_category(df, category)
+    stock_dict = {}
+    for item in items:
+        status = st.selectbox(options = ['In stock', 'Low in stock', 'Out of stock'], index=None, placeholder="Current Stock level...", label_visibility = "collapsed")
+        stock_dict[item] = status
+    return stock_dict
+
 def display_categories(conn, area):
   st.subheader('Pick a Category', divider = 'grey')
   chunk_size = 5
@@ -26,15 +38,17 @@ def set_selected_category(category, area):
     else:
         st.session_state.selected_bar_category = category
 
-def display_kitchen_items():
+def display_kitchen_items(conn):
     if 'selected_kitchen_category' in st.session_state:
         category = st.session_state.selected_kitchen_category
         st.subheader(f'{category}', divider = 'grey')
+        stock_dict = get_stock_dict(conn, 'Kitchen', category)
 
-def display_bar_items():
+def display_bar_items(conn):
     if 'selected_bar_category' in st.session_state:
         category = st.session_state.selected_bar_category
         st.subheader(f'{category}', divider = 'grey')
+        stock_dict = get_stock_dict(conn, 'Bar', category)
 
 if __name__ == "__main__":
   st.header('Inventory')
