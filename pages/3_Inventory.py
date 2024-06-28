865835e2fdf47a519a9e09032f35fb38ece7623a
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 20:43:27 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -4,10 +4,14 @@ from auxillaries import *
 def split_list(input_list, chunk_size=5):
     return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
 
-def display_categories():
+def get_categories(conn):
+  df = get_df(conn, 'Kitchen_Inventory')
+  return get_columns(df)
+
+def display_categories(conn):
   st.subheader('Pick a Category', divider = 'grey')
   chunk_size = 5
-  categories = ['Apple', 'Orange', 'Mango', 'Banana', 'Guava', 'Jackie', 'Passionfruit']
+  categories = get_categories(conn)
   list_categories = split_list(categories, chunk_size)
   with st.container(border = True):
     for cat in list_categories:
@@ -23,12 +27,11 @@ def display_items():
     if 'selected_category' in st.session_state:
         category = st.session_state.selected_category
         st.subheader(f'{category}', divider = 'grey')
-        st.dataframe(get_df())
-
 
 if __name__ == "__main__":
   st.header('Inventory')
-  display_categories()
+  conn = get_connection()
+  display_categories(conn)
   display_items()
   
 
