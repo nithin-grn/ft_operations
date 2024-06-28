c7372aaf8f55b9a25c318874b4d65043ca045013
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 18:41:10 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+def display_categories():
+  categories = ['Apple', 'Orange', 'Mango', 'Banana', 'Guava', 'Jackie', 'Passionfruit']
+  cols = st.columns(len(categories))
+  for idx, col in enumerate(cols):
+    col1.image('logo.jpg')
+    col2.write(categories[idx])
+
 if __name__ == "__main__":
   st.header('Inventory')
+  display_categories()
