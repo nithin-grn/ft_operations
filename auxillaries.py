e0bb6209d8fceab8fa3c54b7ac84c02a577fdc24
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 20:40:02 +1000

    Update auxillaries.py

--- auxillaries.py
+++ auxillaries.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-def get_df():
+def get_connection():
   # Create a connection object.
   conn = st.connection("gsheets", type=GSheetsConnection)
+  return conn
   
+def get_df(conn, sheet_name):
   df = conn.read(
-      worksheet="Sheet1",
-      ttl="10m",
-      usecols=[0, 1],
-      nrows=3,
+      worksheet=sheet_name,
+      ttl="10m"
   )
   return df
+
+def get_columns(df):
+  return df.columns.tolist()
