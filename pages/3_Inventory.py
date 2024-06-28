4c54c370046e5968dbcacfacb2c65a10c5854361
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-06-28 18:59:38 +1000

    Update 3_Inventory.py

--- pages/3_Inventory.py
+++ pages/3_Inventory.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
+def split_list(input_list, chunk_size=5):
+    return [input_list[i:i + chunk_size] for i in range(0, len(input_list), chunk_size)]
+
 def display_categories():
   categories = ['Apple', 'Orange', 'Mango', 'Banana', 'Guava', 'Jackie', 'Passionfruit']
+  list_categories = split_list(categories)
   with st.container():
-    cols = st.columns(len(categories))
-    for idx, col in enumerate(cols):
-      col.image('logo.jpg')
-      col.button(categories[idx])
+    for cat in list_categories:
+      cols = st.columns(len(cat))
+      for idx, col in enumerate(cols):
+        col.image('logo.jpg')
+        col.button(categories[idx])
 
 if __name__ == "__main__":
   st.header('Inventory')
