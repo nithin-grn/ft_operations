278118f2bbfc2c5a2fcf69839b653ac41c454eaa
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-04 19:59:18 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,8 +37,8 @@ def check_reminders():
 
 def inv_reminder():
   day = what_day(today_date_string())
-  # if day in ['Wednesday', 'Sunday']:
-  reminders.append('Please ensure to update the inventory today.')
+  if day in ['Wednesday', 'Sunday']:
+    reminders.append('Please ensure to update the inventory today.')
 
 def display_reminders():
   if len(reminders) > 0:
