11a1e0a2460493b5bf13b7253f265b7a4cd3749d
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-10 03:47:19 +1000

    Update auxillaries.py

--- auxillaries.py
+++ auxillaries.py
@@ -9,8 +9,9 @@ def get_tasks(filename):
 
 def label_activity(df, time):
   today = today_date_string()
-  st.write(type(df['Date'].values[0]))
-  if today in df['Date'].values:
+  st.write(today)
+  # st.write(type(df['Date'].values[0]))
+  if today in list(df['Date'].values):
     st.write('Yes')
     df.loc[df['Date'] == today, time] = 'Done'
   st.write(df)
