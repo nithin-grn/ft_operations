4cc420d5e5f90d8b4a8e4c8414afffb23234deba
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:52:49 +1000

    Update 1_Opening_Tasks.py

--- pages/1_Opening_Tasks.py
+++ pages/1_Opening_Tasks.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import auxillaries as ax
 
-def submit_update():
-  st.write("Submitted!")
-
 if __name__ == "__main__":
   st.header('Opening Tasks')
   tasks = ax.get_tasks('opening.txt')
