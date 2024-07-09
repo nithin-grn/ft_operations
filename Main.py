ade04bc533e06f120aa1d577db13c09b9bf8017f
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:03:21 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,18 @@ from st_clickable_images import clickable_images
 from st_pages import show_pages_from_config
 from auxillaries import *
 
+st.set_page_config(
+    page_title="FT Operations",
+    page_icon="🗒️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        # 'Get Help': 'https://www.extremelycoolapp.com/help',
+        # 'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 st.logo('logo.jpg')
 st.header('FT Local Kitchen & Bar', divider = 'grey')
 st.sidebar.header('FT Local Kitchen & Bar')
