15c60fb8763fcebab4e493d4991df843a9536b6e
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:52:33 +1000

    Update 2_Closing_Tasks.py

--- pages/2_Closing_Tasks.py
+++ pages/2_Closing_Tasks.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import auxillaries as ax
 
-def submit_update():
-  st.write("Submitted!")
-
 if __name__ == "__main__":
   st.header('Closing Tasks')
   tasks = ax.get_tasks('closing.txt')
