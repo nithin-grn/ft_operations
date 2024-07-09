95242b48e5c62590c722e7c78f5b55c914f1241b
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-10 03:37:36 +1000

    Update 1_Opening_Tasks.py

--- pages/1_Opening_Tasks.py
+++ pages/1_Opening_Tasks.py
@@ -4,4 +4,4 @@ import auxillaries as ax
 if __name__ == "__main__":
   st.header('Opening Tasks')
   tasks = ax.get_tasks('opening.txt')
-  ax.display_tasks(tasks)
+  ax.display_tasks(tasks 'Opening Tasks')
