9a5adc341e562dfeaf4af3ae4a225f1722b41704
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:51:01 +1000

    Update 1_Opening_Tasks.py

--- pages/1_Opening_Tasks.py
+++ pages/1_Opening_Tasks.py
@@ -1,38 +1,10 @@
 import streamlit as st
-
-def get_tasks():
-  with open('opening.txt', 'r') as f:
-    tasks = [task.strip() for task in f.readlines()]
-  return tasks
+import auxillaries as ax
 
 def submit_update():
   st.write("Submitted!")
 
-def display_tasks(tasks):
-  for idx, task in enumerate(tasks):
-    with st.container(border = True):
-      name, description = task.split('-')
-      checked = st.checkbox(f":red-background[Task {idx + 1}: {name}]")
-      st.write(description)
-    if checked:
-      if idx + 1 == len(tasks):
-        if st.button("Submit", type = "primary", use_container_width = True):
-          submit_update()
-      continue
-    else:
-      break
-
-# def display_tasks(tasks):
-#   st.write(tasks)
-#   for idx, task in enumerate(tasks):
-#     name, description = task.split('-')
-#     st.subheader(f"Task {idx + 1}: {name.strip()}")
-#     st.write(f"{description}")
-#     if st.button(f"Task {idx + 1} Completed"):
-#       continue
-#     break
-
 if __name__ == "__main__":
   st.header('Opening Tasks')
-  tasks = get_tasks()
-  display_tasks(tasks)
+  tasks = ax.get_tasks('opening.txt')
+  ax.display_tasks(tasks)
