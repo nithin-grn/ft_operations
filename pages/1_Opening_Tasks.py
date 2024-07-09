c8f0259d9c1a916754610490c2d75fd0614dc676
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:10:03 +1000

    Update 1_Opening_Tasks.py

--- pages/1_Opening_Tasks.py
+++ pages/1_Opening_Tasks.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
+def get_tasks():
+  with open('opening.txt', 'r') as f:
+    tasks = [task.strip() for task in f.readlines()]
+  return tasks
+
+def display_tasks(tasks):
+  for task in tasks:
+    if st.checkbox(task):
+      continue
+
 if __name__ == "__main__":
   st.header('Opening Tasks')
