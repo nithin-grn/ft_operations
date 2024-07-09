728756af3392d61777bf8a0092f88a6543f360e5
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-09 20:27:27 +1000

    Update 1_Opening_Tasks.py

--- pages/1_Opening_Tasks.py
+++ pages/1_Opening_Tasks.py
@@ -5,22 +5,25 @@ def get_tasks():
     tasks = [task.strip() for task in f.readlines()]
   return tasks
 
-# def display_tasks(tasks):
-#   for task in tasks:
-#     if st.checkbox(task):
-#       continue
-#     else:
-#       break
-
 def display_tasks(tasks):
-  st.write(tasks)
-  for idx, task in enumerate(tasks):
+  for task in tasks:
     name, description = task.split('-')
-    st.subheader(f"Task {idx + 1}: {name.strip()}")
-    st.write(f"{description}")
-    if st.button(f"Task {idx + 1} Completed"):
+    checked = st.checkbox(name)
+    st.write(description)
+    if checked:
       continue
-    break
+    else:
+      break
+
+# def display_tasks(tasks):
+#   st.write(tasks)
+#   for idx, task in enumerate(tasks):
+#     name, description = task.split('-')
+#     st.subheader(f"Task {idx + 1}: {name.strip()}")
+#     st.write(f"{description}")
+#     if st.button(f"Task {idx + 1} Completed"):
+#       continue
+#     break
 
 if __name__ == "__main__":
   st.header('Opening Tasks')
