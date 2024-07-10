a81cc8df2db7a8c4254412513b9f495bf50f9eb4
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-10 21:20:45 +1000

    Update 3_Offers_and_Discounts.py

--- pages/3_Offers_and_Discounts.py
+++ pages/3_Offers_and_Discounts.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+def display_offer(name, when, desc):
+    with st.container(border = True):
+        st.title(name)
+        st.divider()
+        st.header(when)
+        st.subheader(desc)
+
 if __name__ == "__main__":
     st.header('Offers and Discounts')
+    c1, c2 = st.columns(2)
+    with c1:
+        display_offer('Buy 1 Get 1', 'Tuesday to Thursday', 'Buy 1 Get 1 free burger')
+    with c2:
+        display_offer('Beer Discount', 'Friday to Sunday', '50% Flat off on Tap Beer')
