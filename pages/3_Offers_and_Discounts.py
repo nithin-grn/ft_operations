a23b96b3f245f6bf243cdd4e57386b5fc68accbd
nithin-grn <nithingangadharanrangaraj@gmail.com>
2024-07-11 13:44:08 +1000

    Update 3_Offers_and_Discounts.py

--- pages/3_Offers_and_Discounts.py
+++ pages/3_Offers_and_Discounts.py
@@ -15,7 +15,7 @@ def display_offer(name, when, desc):
 
 if __name__ == "__main__":
     st.header('Offers and Discounts', divider = 'grey')
-    c1, c2, c3 = st.columns(3, vertical_alignment = 'top')
+    c1, c2, c3, c4, c5 = st.columns(3, vertical_alignment = 'top')
     with c1:
         display_offer('Buy 1 Get 1', 'Tuesday to Thursday', 'Buy 1 Get 1 free burger')
     with c2:
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     with c3:
         display_offer('Happy Hour', 'All days 4-7 pm', 'Description')
 
-    c1, c2 = st.columns(2, vertical_alignment = 'top')
-    with c1:
+    # c1, c2 = st.columns(2, vertical_alignment = 'top')
+    with c4:
         display_offer('Kids Day!', 'Sunday 4 pm', 'Kids Movie & Kids Eat for free')
-    with c2:
+    with c5:
         display_offer('Footy Screening', 'Friday 6:30 pm', 'Description')
